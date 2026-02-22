6fe1d8bc7cfef700ea0ea46be3034877dcb386f2
Max Zhang <142354778+maxh119Z@users.noreply.github.com>
2026-02-21 19:27:20 -0800

    embedding COMET

--- reproduce/2_embedding.sh
+++ reproduce/2_embedding.sh
@@ -3,5 +3,8 @@ set -euo pipefail
 cd "$(dirname "$0")/.."
 source venv_safety_irt/bin/activate
 
-python model/embedding_analysis_translation_v_DIF.py
-python model/embedding_analysis_translation_v_safety.py
+python model/embedding_analysis_translation_v_DIF_LaBSE.py
+python model/embedding_analysis_translation_v_safety_LaBSE.py
+
+python model/embedding_analysis_translation_v_DIF_COMET.py
+python model/embedding_analysis_translation_v_safety_COMET.py
